d51645301388f9ddc29c46b721f509f98f13ebea
sudhanshu0306 <chaturvedi0306@gmail.com>
2024-09-22 14:26:10 +0530

    Changes done

--- main.py
+++ main.py
@@ -26,6 +26,19 @@ try:
 except KeyError:
     SOME_SECRET = "Token not available!"
 
+file = 'output.csv'
+if file in os.listdir('Final_Output'):
+    df_main = pd.read_csv(f'Final_Output\{file}')
+    df_add = pd.DataFrame(columns=['Date','Message'])
+    df_add['Date'] = [formatted_date_time]
+    df_add['Message'] = 'Data added'
+    df_main = pd.concat([df_main,df_add],ignore_index=True).reset_index(drop=True)
+else:
+    df_main = pd.DataFrame(columns=['Date','Message'])
+    df_main['Date'] = [formatted_date_time]
+    df_main['Message'] = 'Data added'
+
+
 df_out = pd.DataFrame()
 df_out[['Date','Message']] = [[formatted_date_time,'File added']]
 
@@ -34,7 +47,7 @@ if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
     logger.info(f"Data appended to the file - {formatted_date_time}")
     SOME_SECRET = "Checking this line of code"
-    df_out.to_csv('Final_Output/output.csv',index=False)
+    df_main.to_csv('Final_Output/output.csv',index=False)
 
 
 
